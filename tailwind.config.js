009a17770775d764d4f6299444109535721dc811
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2024-02-09 18:10:17 +0600

    first picture of Tasker App

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,14 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      container: {
+        center: true,
+      },
+      colors: {
+        clifford: "#da373d",
+      },
+    },
   },
   plugins: [],
 }
