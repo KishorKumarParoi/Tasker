6f117bdb19e3889353c550e495eea86807244014
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2024-02-12 03:08:58 +0600

    Close Button enabled

--- src/App.jsx
+++ src/App.jsx
@@ -111,74 +111,7 @@ function Tasklist({ Tasks, onEdit, onDelete, onFavorite }) {
   )
 }
 
-// function UpdateModal({ task, onEditSave }) {
-//   const [newTask, setNewTask] = useState(task);
-
-
-//   const handleChange = (event) => {
-//     let name = event.target.name;
-//     let value = event.target.value;
-
-//     if (name === 'tags') {
-//       value = value.split(",");
-//     }
-
-//     setNewTask({
-//       ...newTask,
-//       [name]: value,
-//     })
-//   }
-
-//   return (
-//     <>
-//       <div className='bg-black bg-opacity-70 h-full w-full z-5 absolute top-0 left-0 '></div>
-//       <form
-//         className="mx-auto my-10 w-full max-w-[740px] rounded-xl border border-[#FEFBFB]/[36%]
-//          bg-[#191D26] p-9 max-md:px-4 lg:my-20 lg:p-11 z-10 absolute  top-1/3 left-1/4 ">
-//         <h2 className="mb-9 text-center text-2xl font-bold text-white lg:mb-11 lg:text-[28px]">
-//           Add New Task
-//         </h2>
-
-//         <div className="space-y-9 text-white lg:space-y-10">
-//           <div className="space-y-2 lg:space-y-3">
-//             <label htmlFor="title">Title</label>
-//             <input className="block w-full rounded-md bg-[#2D323F] px-3 py-2.5" type="text" value={newTask.title} onChange={handleChange} name="title" id="title"
-//               required />
-//           </div>
-//           <div className="space-y-2 lg:space-y-3">
-//             <label htmlFor="description">Description</label>
-//             <textarea className="block min-h-[120px] w-full rounded-md bg-[#2D323F] px-3 py-2.5 lg:min-h-[180px]"
-//               type="text" name="description" id="description" value={newTask.description} onChange={handleChange} required></textarea>
-//           </div>
-//           <div className="grid-cols-2 gap-x-4 max-md:space-y-9 md:grid lg:gap-x-10 xl:gap-x-20">
-//             <div className="space-y-2 lg:space-y-3">
-//               <label htmlFor="tags">Tags</label>
-//               <input className="block w-full rounded-md bg-[#2D323F] px-3 py-2.5" type="text" value={newTask.tags} onChange={handleChange} name="tags" id="tags"
-//                 required />
-//             </div>
-//             <div className="space-y-2 lg:space-y-3">
-//               <label htmlFor="priority">Priority</label>
-//               <select className="block w-full cursor-pointer rounded-md bg-[#2D323F] px-3 py-2.5" name="priority"
-//                 id="priority" value={newTask.priority} onChange={handleChange} required>
-//                 <option value="">Select Priority</option>
-//                 <option value="Low">Low</option>
-//                 <option value="Medium">Medium</option>
-//                 <option value="High">High</option>
-//               </select>
-//             </div>
-//           </div>
-//         </div>
-//         <div className="mt-16 flex justify-center lg:mt-20">
-//           <button type="submit" className="rounded bg-blue-600 px-4 py-2 text-white transition-all hover:opacity-80" onClick={(event) => { onEditSave(newTask); event.preventDefault(); }}>
-//             Save
-//           </button>
-//         </div>
-//       </form>
-//     </>
-//   )
-// }
-
-function AddModal({ onSave, isEdit, editableTask }) {
+function AddModal({ onSave, isEdit, editableTask, onClose }) {
   const [task, setTask] = useState(editableTask || {
     "id": crypto.randomUUID(),
     "title": "",
@@ -263,7 +196,10 @@ function AddModal({ onSave, isEdit, editableTask }) {
             </div>
           </div>
         </div>
-        <div className="mt-16 flex justify-center lg:mt-20">
+        <div className="mt-16 flex justify-between lg:mt-20">
+          <button type='submit' className="rounded bg-red-600 px-4 py-2 text-white transition-all hover:opacity-80" onClick={(event) => { event.preventDefault(); onClose(); }} >
+            Close
+          </button>
           <button type="submit" className="rounded bg-blue-600 px-4 py-2 text-white transition-all hover:opacity-80" onClick={(event) => {
             handleSubmit(event);
           }} >
@@ -327,7 +263,10 @@ function TaskBoard() {
     setShowAddModal(false);
   }
 
-  // const [newEditableTask, setNewEditableTask] = useState(demoData);
+  function handleCloseClick() {
+    console.log('Closing ....');
+    setShowAddModal(false);
+  }
 
   function handleEditTask(task) {
     console.log('editing a task...');
@@ -335,25 +274,7 @@ function TaskBoard() {
     // console.log(task);
     setIsEdit(true);
     setShowAddModal(true);
-    // setNewEditableTask(task);
-    // setShowUpdateModal(true);
   }
-
-  // function handleEditSave(task) {
-  //   console.log("🚀 ~ handleEditSave ~ task:", task);
-
-  //   const newTasks = Tasks.map((t) => {
-  //     if (t.id === task.id)
-  //       return task;
-  //     else
-  //       return t;
-  //   });
-
-  //   console.log("🚀 ~ newTasks ~ newTasks:", newTasks);
-  //   setTasks(newTasks);
-  //   // setShowUpdateModal(false);
-  //   console.log('Edited and now to be saved...');
-  // }
 
   function handleDeleteTask(task) {
     console.log("Deleting a task...");
@@ -410,7 +331,7 @@ function TaskBoard() {
   return (
     <>
       <section className="mb-20" id="tasks">
-        {showAddModal && <AddModal onSave={handleAddAndEditTask} isEdit={isEdit} editableTask={editableTask} />}
+        {showAddModal && <AddModal onSave={handleAddAndEditTask} isEdit={isEdit} editableTask={editableTask} onClose={handleCloseClick} />}
         {/* {showUpdateModal && <UpdateModal task={newEditableTask} onEditSave={handleEditSave} />} */}
 
         <div className="container">
